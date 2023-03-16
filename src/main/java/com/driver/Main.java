0657b3b376b8115367009425507911bb667de000
rajaram <rajaram@runloyal.com>
2023-03-16 21:05:06 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    static class Product{
+        Product(){
+
+        }
+        public int product(int x , int y){
+            return x*y;
+        }
+        public int product(int x, int y, int z){
+            return x*y*z;
+        }
+        public double product(double x , double y){
+            return x*y;
+        }
+    }
+    public static void main(String[] args) {
+        Product p = new Product();
+
+       int a= p.product(24,9);
+
+       int b= p.product(24,9, 23);
+
+       double c = p.product(24.0,9.0);
+       System.out.print(a+" "+b+" "+c);
+    }
 
 }
